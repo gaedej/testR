3d4f20bb5a6a4aa0ec8d546a08fa7e094b1bfb81
Joel Gaede <joelgaede@Top-Secret-Evil-Access-Point.local>
2012-06-14 22:04:09 -0500

    Added Labels

--- my_testR.R
+++ my_testR.R
@@ -1,9 +1,10 @@
 par( mfrow = c( 2, 3 ) )
+
 x <- c(1:100)
-plot(x, type="l")
+plot(x, type="l",main = "plot 1", cex.lab = 1.1)
 
-plot(x^2, type="l", col="red")
+plot(x^2, type="l", col="red", main = "plot 2", cex.lab = 1.1)
 
-plot(x^3, type="l", col="green")
+plot(x^3, type="l", col="green", main = "plot 3", cex.lab = 1.1)
 
-plot(x^3, type="l", col="green")
+plot(x^3, type="l", col="green", main = "plot 4", cex.lab = 1.1)
