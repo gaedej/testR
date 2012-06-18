aaf99e532b8c94203cde3da36e600b9a974cb0a7
Joel Gaede <joelgaede@Top-Secret-Do-Not-Hack.local>
2012-06-18 10:15:59 -0500

    Added Math font script

--- my_testR.R
+++ my_testR.R
@@ -1,3 +1,4 @@
+library(ggplot2)
 par( mfrow = c( 2, 3 ) )
 # Notes for printout.
 
@@ -9,3 +10,8 @@ plot(x^2, type="l", col="red", main = "plot 2", cex.lab = 1.1)
 plot(x^3, type="l", col="green", main = "plot 3", cex.lab = 1.1)
 
 plot(x^3, type="l", col="green", main = "plot 4", cex.lab = 1.1)
+
+plot(x^3, type="l", col="green", main = "plot 4", cex.lab = 1.1)
+
+qplot(1:10, 1:10) + opts(title = sprintf("$\\mathrm{d}\\mathbf{x}_{t} = \\alpha[(\\theta
+- \\mathbf{x}_{t})\\mathrm{d}t + %d]\\mathrm{d}B_{t}$", 4))
