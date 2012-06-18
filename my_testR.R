5d19e26fdb1383f0db607c2ac35775587927d68d
Joel Gaede <joelgaede@Top-Secret-Do-Not-Hack.local>
2012-06-18 10:38:29 -0500

    Added Headings to Script

--- my_testR.R
+++ my_testR.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 par( mfrow = c( 2, 3 ) )
 # Notes for printout.
-
+# Columns of plots
 x <- c(1:100)
 plot(x, type="l",main = "plot 1", cex.lab = 1.1)
 
@@ -13,5 +13,6 @@ plot(x^3, type="l", col="green", main = "plot 4", cex.lab = 1.1)
 
 plot(x^3, type="l", col="green", main = "plot 4", cex.lab = 1.1)
 
+# Math Notation Header
 qplot(1:10, 1:10) + opts(title = sprintf("$\\mathrm{d}\\mathbf{x}_{t} = \\alpha[(\\theta
 - \\mathbf{x}_{t})\\mathrm{d}t + %d]\\mathrm{d}B_{t}$", 4))
